b239dc7900666a890f4d850b166e607ad394d35a
Andre Dias <andre.gustavo@zig.fun>
2024-06-01 14:02:24 -0300

    feat: initial setup

---
+++ swiftUICryptoApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  swiftUICryptoApp
+//
+//  Created by Andre Dias on 01/06/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ swiftUICryptoApp/swiftUICryptoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftUICryptoAppApp.swift
+//  swiftUICryptoApp
+//
+//  Created by Andre Dias on 01/06/24.
+//
+
+import SwiftUI
+
+@main
+struct swiftUICryptoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
