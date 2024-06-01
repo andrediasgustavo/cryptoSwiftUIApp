8de514e1f29a0973f3a3ed1c86dbf44bad5329a3
Andre Dias <andre.gustavo@zig.fun>
2024-06-01 14:29:24 -0300

    feat: setup colors and add some images

---
+++ swiftUICryptoApp/Extensions/Color.swift
@@ -0,0 +1,21 @@
+//
+//  Color.swift
+//  swiftUICryptoApp
+//
+//  Created by Andre Dias on 01/06/24.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    static let theme = ColorTheme()
+}
+
+struct ColorTheme {
+    let accent = Color("AccentColor")
+    let background = Color("BackgroundColor")
+    let green = Color("GreenColor")
+    let red = Color("RedColor")
+    let secondaryText = Color("SecondaryTextColor")
+}
