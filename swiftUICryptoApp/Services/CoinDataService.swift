41e2cb07a12799a768e499d76300270f99d62f9f
Andre Dias <andrediasgustavo@gmail.com>
2024-06-04 13:32:52 -0300

    feat: Refactor to make networking api call more generic

--- swiftUICryptoApp/Services/CoinDataService.swift
+++ swiftUICryptoApp/Services/CoinDataService.swift
@@ -20,28 +20,13 @@ class CoinDataService {
     private func getCoins() {
         guard let url = URL(string: "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&page=1&sparkline=true&price_change_percentage=24h") else { return }
         
-        coinSubscription = URLSession.shared.dataTaskPublisher(for: url)
-            .subscribe(on: DispatchQueue.global(qos: .default))
-            .tryMap { output -> Data in
-                guard let response = output.response as? HTTPURLResponse,
-                response.statusCode >= 200 &&  response.statusCode < 300 else {
-                    throw URLError(.badServerResponse)
-                }
-                return output.data
-            }
-            .receive(on: DispatchQueue.main)
+        coinSubscription = NetworkingManager.download(url: url)
             .decode(type: [CoinModel].self, decoder: JSONDecoder())
-            .sink { completion in
-                switch completion {
-                case .finished: break
-                case .failure(let error):
-                    print(error.localizedDescription)
-                }
-            } receiveValue: { [weak self] coinsList in
+            .sink(receiveCompletion:  NetworkingManager.handleCompletion,
+                  receiveValue: { [weak self] coinsList in
                 guard let self = self else { return }
                 self.allCoins = coinsList
                 self.coinSubscription?.cancel()
-            }
-
+            })
     }
 }
