c26e9921433f594aa9162eb1b780699385cae6a3
Andre Dias <andrediasgustavo@gmail.com>
2024-06-27 00:45:23 -0300

    feat: Setup detail view sections and fill it with info from backend

--- swiftUICryptoApp/Core/Detail/Views/DetailView.swift
+++ swiftUICryptoApp/Core/Detail/Views/DetailView.swift
@@ -24,20 +24,82 @@ struct DetailLoadingView: View {
 
 struct DetailView: View {
     
-    @StateObject var viewModel: DetailViewModel
+    @StateObject private var viewModel: DetailViewModel
+    private let collumns: [GridItem] = [
+        GridItem(.flexible()),
+        GridItem(.flexible())
+    ]
+    let spacing: CGFloat = 30
    
     init(coin: CoinModel) {
         self._viewModel = StateObject(wrappedValue: DetailViewModel(coin: coin))
     }
     
     var body: some View {
-        Text("Hello")
+        ScrollView {
+            VStack(spacing: 20) {
+                Text(viewModel.coin.name)
+                overviewTitle()
+                Divider()
+                overViewGrid()
+                additionalTitle()
+                Divider()
+                additionalGrid()
+            }
+            .padding()
+        }
+        .navigationTitle(viewModel.coin.name)
+    }
+}
+
+extension DetailView {
+    
+    private func overviewTitle() -> some View {
+        Text("Overview")
+            .font(.title)
+            .bold()
+            .foregroundColor(Color.theme.accent)
+            .frame(maxWidth: .infinity, alignment: .leading)
+    }
+    
+    private func additionalTitle() -> some View {
+        Text("Additional Details")
+            .font(.title)
+            .bold()
+            .foregroundColor(Color.theme.accent)
+            .frame(maxWidth: .infinity, alignment: .leading)
+    }
+    
+    private func overViewGrid() -> some View {
+        LazyVGrid(columns: collumns,
+                  alignment: .leading,
+                  spacing: spacing,
+                  pinnedViews: /*@START_MENU_TOKEN@*/[]/*@END_MENU_TOKEN@*/,
+                  content: {
+            ForEach(viewModel.overviewStatistics) { stats in
+                StatisticView(stat: stats)
+            }
+        })
+    }
+    
+    private func additionalGrid() -> some View {
+        LazyVGrid(columns: collumns,
+                  alignment: .leading,
+                  spacing: spacing,
+                  pinnedViews: /*@START_MENU_TOKEN@*/[]/*@END_MENU_TOKEN@*/,
+                  content: {
+            ForEach(viewModel.additionalStatistics) { stats in
+                StatisticView(stat: stats)
+            }
+        })
     }
 }
 
 
 struct DetailView_Previews: PreviewProvider {
     static var previews: some View {
-        DetailView(coin: dev.coin)
+        NavigationView {
+            DetailView(coin: dev.coin)
+        }
     }
 }
