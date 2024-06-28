ff3e6351ffe628516c3890e228e5e8c8e067d027
Andre Dias <andrediasgustavo@gmail.com>
2024-06-27 22:45:45 -0300

    feat: Create chartview for detail view

--- swiftUICryptoApp/Core/Detail/Views/DetailView.swift
+++ swiftUICryptoApp/Core/Detail/Views/DetailView.swift
@@ -37,22 +37,42 @@ struct DetailView: View {
     
     var body: some View {
         ScrollView {
-            VStack(spacing: 20) {
-                Text(viewModel.coin.name)
-                overviewTitle()
-                Divider()
-                overViewGrid()
-                additionalTitle()
-                Divider()
-                additionalGrid()
+            VStack {
+                ChartView(coin: viewModel.coin)
+                    .padding(.vertical)
+                
+                VStack(spacing: 20) {
+                    
+                    overviewTitle()
+                    Divider()
+                    overViewGrid()
+                    additionalTitle()
+                    Divider()
+                    additionalGrid()
+                }
+                .padding()
             }
-            .padding()
         }
         .navigationTitle(viewModel.coin.name)
+        .toolbar {
+            ToolbarItem(placement: .navigationBarTrailing) {
+                navigationBarTrailingItems()
+            }
+        }
     }
 }
 
 extension DetailView {
+    
+    private func navigationBarTrailingItems() -> some View {
+        HStack {
+            Text(viewModel.coin.symbol.uppercased())
+                .font(.headline)
+                .foregroundColor(Color.theme.secondaryText)
+            CoinImageView(coin: viewModel.coin)
+                .frame(width: 25, height: 25)
+        }
+    }
     
     private func overviewTitle() -> some View {
         Text("Overview")
