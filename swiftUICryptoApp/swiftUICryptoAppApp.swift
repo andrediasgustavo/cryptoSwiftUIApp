8a83b6e34fb245455f88165a54be3e4972f29e91
Andre Dias <andrediasgustavo@gmail.com>
2024-06-29 13:06:41 -0300

    feat: create launch view with animations and adjsut for ipad

--- swiftUICryptoApp/swiftUICryptoAppApp.swift
+++ swiftUICryptoApp/swiftUICryptoAppApp.swift
@@ -11,6 +11,7 @@ import SwiftUI
 struct swiftUICryptoAppApp: App {
 
     @StateObject private var vm = HomeViewModel()
+    @State private var showLaunchView: Bool = true
     
     init() {
         UINavigationBar.appearance().largeTitleTextAttributes = [.foregroundColor: UIColor(Color.theme.accent)]
@@ -19,11 +20,22 @@ struct swiftUICryptoAppApp: App {
     
     var body: some Scene {
         WindowGroup {
-            NavigationView {
-                HomeView()
-                    .navigationBarHidden(true)
+            ZStack {
+                NavigationView {
+                    HomeView()
+                        .navigationBarHidden(true)
+                }
+                .navigationViewStyle(StackNavigationViewStyle())
+                .environmentObject(vm)
+                
+                ZStack {
+                    if showLaunchView {
+                        LaunchView(showLaunchView: $showLaunchView)
+                            .transition(.move(edge: .leading))
+                    }
+                }
+                .zIndex(2.0)
             }
-            .environmentObject(vm)
         }
     }
 }
